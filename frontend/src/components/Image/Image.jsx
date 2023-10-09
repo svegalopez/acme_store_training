2b632f926278c3e6177eeaf4eb82794f01f4e44e
Sebastian Vega <svegalopez@gmail.com>
2023-10-09 11:35:57 -0400

    Image

--- frontend/src/components/Image/Image.jsx
+++ frontend/src/components/Image/Image.jsx
@@ -1,3 +1,20 @@
-export default function Img() {
-  return "Img";
+import React from "react";
+import styles from "./Image.module.css";
+
+export default function Img({ src, alt, className, aspectRatio }) {
+  let classes = styles.img;
+
+  if (className) {
+    classes += " " + className;
+  }
+
+  return (
+    <img
+      loading="lazy"
+      style={{ aspectRatio: aspectRatio || "1/1" }}
+      className={classes}
+      src={src}
+      alt={alt}
+    ></img>
+  );
 }
